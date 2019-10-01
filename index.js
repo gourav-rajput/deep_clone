8a7275ad7c2c131bec268085f59ee5ed9e4598a0
gouravr-94 <gouravr.94@gmail.com>
2019-10-01 17:44:56 +0530

    final functioanlity

--- index.js
+++ index.js
@@ -17,3 +17,59 @@ const isArray = ele => {
 const isObject = ele => {
   return ele && typeof ele === 'object' && Array.isArray(ele) === false;
 }
+
+const handleArray = data => {
+  return data.map(ele => {
+    return cloneDeep(ele);
+  });
+}
+
+const handleObject = data => {
+  let newObject = {};
+  for(let key in data) {
+    newObject = {
+      ...newObject,
+      [key]: cloneDeep(data[key]),
+    };
+  }
+  return newObject
+};
+
+cloneDeep = data => {
+  if(isString(data)) return String(data);
+  
+  if(isNumber(data)) return Number(data);
+  
+  if(isFunction(data)) return data.bind({});
+  
+  if(isArray(data)){
+    return handleArray(data);
+  }
+  
+  if(isObject(data)){
+    return handleObject(data);
+  }
+};
+
+
+const clonedData = cloneDeep("wd");
+
+let obj = {
+  "someKey": "someValue",
+  "someOtherKey": "someOtherValue",
+  "someArrayValue": [12,34,56,78],
+  "someDeepArray": [
+    {"someKeyInDeepArray": "try to change this"},
+    {"someOtherKeyInDeepArray": "...rest"}
+  ]
+};
+
+
+const clonedObject = cloneDeep(obj)
+
+console.log("clonedObject----->", clonedObject);
+clonedObject.someDeepArray[0].someKeyInDeepArray = "changed value";
+
+console.log("modified clonedObject----->", clonedObject);
+
+console.log("Origional Object------>", obj);
